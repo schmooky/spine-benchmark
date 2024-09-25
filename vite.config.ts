1f92fd2a50407e039d53303afeb374aa6b669a87
Dmitry Vasiliev <dmitry.vasiliev@schmooky.dev>
2024-09-25 17:37:54 +0300

    feat: change build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig(async ({ mode }) => {
 
@@ -6,6 +7,14 @@ export default defineConfig(async ({ mode }) => {
     publicDir: 'assets',
     server: {
       port: 8080,
+    },
+    build: {
+      target: 'esnext',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+      },
     },
   };
 });
